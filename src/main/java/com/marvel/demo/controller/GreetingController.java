aa61be181d0142db7221204fc39baf2bd9667c39
程孟威 <chengmengwei@iplas.com.cn>
2018-09-12 15:48:58 +0800

    first commit

---
+++ src/main/java/com/marvel/demo/controller/GreetingController.java
@@ -0,0 +1,25 @@
+package com.marvel.demo.controller;
+
+import com.marvel.demo.domain.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Marvel on 18/09/12.
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+}
