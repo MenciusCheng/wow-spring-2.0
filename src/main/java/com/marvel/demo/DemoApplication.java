35b75c84b348f0baf7dcf18418db27bb3a2a83d6
程孟威 <chengmengwei@iplas.com.cn>
2018-09-29 15:47:04 +0800

    add web, mysql

--- src/main/java/com/marvel/demo/DemoApplication.java
+++ src/main/java/com/marvel/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.marvel.demo;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+//		SpringApplication.run(DemoApplication.class, args);
+
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 }
