24849df6bb2bdd885a29cba2d44bd88643ea1a90
程孟威 <chengmengwei@iplas.com.cn>
2018-12-03 11:04:50 +0800

    remove data-jpa

--- src/main/java/com/marvel/demo/domain/CustomerRepository.java
+++ src/main/java/com/marvel/demo/domain/CustomerRepository.java
@@ -1,13 +1,13 @@
-package com.marvel.demo.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-/**
- * Created by Marvel on 18/09/14.
- */
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-
-    List<Customer> findByName(String name);
-}
+//package com.marvel.demo.domain;
+//
+//import org.springframework.data.repository.CrudRepository;
+//
+//import java.util.List;
+//
+///**
+// * Created by Marvel on 18/09/14.
+// */
+//public interface CustomerRepository extends CrudRepository<Customer, Long> {
+//
+//    List<Customer> findByName(String name);
+//}
