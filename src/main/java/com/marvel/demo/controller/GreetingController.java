a0c90e7d52f92e8d9d2c338b85077bcbc8c6ad35
程孟威 <295567172@qq.com>
2019-09-05 10:58:58 +0800

    读取配置

--- src/main/java/com/marvel/demo/controller/GreetingController.java
+++ src/main/java/com/marvel/demo/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.marvel.demo.controller;
 
 import com.marvel.demo.domain.Greeting;
+import com.marvel.demo.property.DatabaseProperty;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +18,22 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    private final DatabaseProperty databaseProperty;
+
+    @Autowired
+    public GreetingController(DatabaseProperty databaseProperty) {
+        this.databaseProperty = databaseProperty;
+    }
+
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
+    }
+
+    @RequestMapping("/greeting2")
+    public Greeting greeting2(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), databaseProperty.getUrl());
     }
 
 }
---
+++ src/main/java/com/marvel/demo/property/DatabaseProperty.java
@@ -0,0 +1,46 @@
+package com.marvel.demo.property;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * 读取属性配置
+ *
+ * Created by Marvel on 2019/9/5.
+ */
+@Component
+public class DatabaseProperty {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
